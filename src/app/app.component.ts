2478a69bcb9e1af4e29b69f6c089ceb63ca715ae
aomine1301 <grooom1301@gmail.com>
2024-07-03 19:37:06 +0300

    first commit 24хв

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ProfileCardComponent} from "./common-ui/profile-card/profile-card.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProfileCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angul';
+
+  user = {
+    name: 'marina',
+    surname: 'frolova'
+  }
+  tooltip = 'це підказка'
 }
